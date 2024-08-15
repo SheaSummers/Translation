4768e0b3d3c4cddabd5830d4be77668f5293960b
Shea Summers <61829927+SheaSummers@users.noreply.github.com>
2024-08-15 14:45:03 -0700

    Update data_format.py

--- data_format.py
+++ data_format.py
@@ -1,6 +1,6 @@
 import torch
-import torchtext;torchtext.disable_torchtext_deprecation_warning()
 import pandas as pd
+import torch.nn as nn
 from torch.nn.utils.rnn import pad_sequence
 from torchtext.vocab import build_vocab_from_iterator
 from torch.utils.data import Dataset, DataLoader
@@ -25,29 +25,33 @@ def build_vocabulary(iter, tokenizer, lang):
     vocab.set_default_index(vocab["<unk>"])
     return vocab
 
-def data_process(data_sample, src_vocab, tgt_vocab, src_tokenizer, tgt_tokenizer):
-
-    print(data_sample[0])
-
-    src = [src_bos] + [src_vocab[token] for token in src_tokenizer(data_sample[0])] + [src_eos]
-    tgt = [tgt_bos] + [tgt_vocab[token] for token in tgt_tokenizer(data_sample[1])] + [tgt_eos]
-    return torch.tensor(src), torch.tensor(tgt)
 
 def generate_batch(data_batch):
-    src_batch, tgt_batch = [], []
-    for src_sample, tgt_sample in data_batch:
-        src_batch.append(src_sample)
-        tgt_batch.append(tgt_sample)
-    src_batch = pad_sequence(src_batch, padding_value=src_pad, batch_first=True)
-    tgt_batch = pad_sequence(tgt_batch, padding_value=tgt_pad, batch_first=True)
-    return src_batch, tgt_batch
+    src_batch, tgt_batch = zip(*data_batch)
+    return torch.stack(src_batch), torch.stack(tgt_batch)
 
 
 
+def get_loader(data, src_vocab, tgt_vocab, src_tokenizer, tgt_tokenizer, batch_size, src_lang, tgt_lang, max_length):
+    def data_process(data_sample):
+        src = torch.tensor([[src_bos] + [src_vocab[token] for token in src_tokenizer(data_sample[src_lang])] + [src_eos]])
+        tgt = torch.tensor([[tgt_bos] + [tgt_vocab[token] for token in tgt_tokenizer(data_sample[tgt_lang])] + [tgt_eos]])
 
-def get_loader(data, src_vocab, tgt_vocab, src_tokenizer, tgt_tokenizer, batch_size):
 
-    prep_data = [data_process(data_sample, src_vocab, tgt_vocab, src_tokenizer, tgt_tokenizer) for data_sample in data]
+        src = nn.ConstantPad1d((0, max_length - src.shape[1]), src_pad)(src)
+
+
+
+        tgt = nn.ConstantPad1d((0, max_length - tgt.shape[1]), tgt_pad)(tgt)
+        src = src.squeeze(0)
+        tgt = tgt.squeeze(0)
+
+        return src, tgt
+
+
+    prep_data = [data_process(row) for _,row in data.iterrows()]
+
+
     dataloader = DataLoader(prep_data, batch_size=batch_size, shuffle=True, collate_fn=generate_batch)
 
 
